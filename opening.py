29ae29818f71deffa5cad7716b8802882d1566f9
Lucas Verrone <101367035+Linkbooks@users.noreply.github.com>
2024-07-29 20:20:02 -0300

    Add files via upload

---
+++ opening.py
@@ -0,0 +1,29 @@
+import pyautogui
+
+pyautogui.PAUSE = 2.0
+pyautogui.PAUSE = True
+
+# abre o firefox #
+pyautogui.hotkey('win')
+pyautogui.write('firefox')
+pyautogui.hotkey('enter')
+
+# abre o brave #
+pyautogui.hotkey('win')
+pyautogui.write('brave')
+pyautogui.hotkey('enter')
+
+# abre o VScode #
+pyautogui.hotkey('win')
+pyautogui.write('code')
+pyautogui.hotkey('enter')
+
+# abre o pyCharm #
+pyautogui.hotkey('win')
+pyautogui.write('pycharm')
+pyautogui.hotkey('enter')
+
+# abre o whatsapp #
+pyautogui.hotkey('win')
+pyautogui.write('whatsapp')
+pyautogui.hotkey('enter')
